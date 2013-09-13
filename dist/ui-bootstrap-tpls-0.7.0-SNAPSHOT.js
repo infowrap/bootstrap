f78de597bf096143b01adc726208bef828bb798b
Nathan Walker <nathan.walker@bizbuilt.com>
2013-09-13 11:21:48 -0700

    chore(build): needed build

--- dist/ui-bootstrap-tpls-0.7.0-SNAPSHOT.js
+++ dist/ui-bootstrap-tpls-0.7.0-SNAPSHOT.js
@@ -1491,7 +1491,7 @@ angular.module('ui.bootstrap.modal', [])
 
         // IW CUSTOM
         // modified to support breadcrumb titled modals
-        if (openedWindows.length === 0) {
+        if (openedWindows.length() === 0) {
           openedWindows.add(modalInstance, {
             deferred: modal.deferred,
             modalScope: modal.scope,
